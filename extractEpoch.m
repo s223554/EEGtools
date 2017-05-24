4c898d14f49622ad07c27ed6ca1bf5fdfb324cce
s2235 <s2235@Qian-T460>
2017-05-24 11:42:58 -0400

    Add exception handling to out of index.

--- extractEpoch.m
+++ extractEpoch.m
@@ -3,7 +3,11 @@ function [ epochs ] = extraceEpoch(data,stim_loc,Fs,time_boundary)
 %   
 epochs = zeros(abs(time_boundary(2)-time_boundary(1))*Fs,length(stim_loc));
 for i = 1:length(stim_loc)
+    try
     epochs(:,i) = data((stim_loc(i)+time_boundary(1)*Fs:(stim_loc(i)+time_boundary(2)*Fs-1)));
+    catch
+        continue
+    end
 end
 
 end
