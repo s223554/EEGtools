68d19b47aed38402953da22ece0e91201c9e2b8b
s223554 <s223554@gmail.com>
2017-08-04 08:52:03 -0400

    add a comment

--- spikeFreqDist/freqdistr.m
+++ spikeFreqDist/freqdistr.m
@@ -1,10 +1,10 @@
-Tstart = 6434753;
-Tend = 8241453;
-Tstartle = 7033643;
+Tstart = 6564130;
+Tend = 7764310;
+Tstartle = 6864479;
 duration  = Tend-Tstart;
-durationbins = 60;
-H = hist(VarName1/1000,floor(duration/1000/durationbins));
-hist(VarName1/1000,floor(duration/1000/durationbins));
+durationbins = 60;  %duration in sec
+H = hist(VarName7/1000,floor(duration/1000/durationbins));
+hist(VarName7/1000,floor(duration/1000/durationbins));
 line([Tstartle/1000 Tstartle/1000],[0 100]);
 ylim([0 30])
 
