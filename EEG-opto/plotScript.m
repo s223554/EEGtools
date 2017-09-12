47bfee082ebed9df194b7fde320a7c1fa8f26d47
s223554 <s223554@gmail.com>
2017-09-11 21:36:27 -0400

    new fixed from git conflict

--- EEG-opto/plotScript.m
+++ EEG-opto/plotScript.m
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 subplot(511)
 plot(tvec,stim_roi)
 xlim([timeStart timeEnd])
@@ -17,26 +16,4 @@ subplot(514)
 bar(theta_delta_ratio);            % the delta raw power.
 xlim([1 size(prev_delta,1)])
 subplot(515)
-bar(locs_data)
-=======
-subplot(511)
-plot(tvec,stim_roi)
-xlim([timeStart timeEnd])
-subplot(512)
-plot_matrix(Spec,t,fspec);
-colormap('jet');
-caxis([-75 -10]);
-colorbar off;
-set(gca,'XTick',[])
-ylabel('Frequency(Hz)')
-xlabel('Time(s)')
-subplot(513)
-plot(tvec,eeg_roi);
-xlim([timeStart timeEnd])
-subplot(514)
-bar(theta_delta_ratio);            % the delta raw power.
-xlim([1 size(prev_delta,1)])
-subplot(515)
-bar(locs_num)
->>>>>>> refs/remotes/origin/master
-xlim([0 range(TB)./bin_size])
+bar(locs_data(:,2))
